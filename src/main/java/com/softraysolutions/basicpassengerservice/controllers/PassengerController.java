a6eefaf0b717496d9770fd8317df56ae6d39553e
Samra <spusina1@etf.unsa.ba>
2021-09-02 18:23:37 +0200

    Implement error handling

--- src/main/java/com/softraysolutions/basicpassengerservice/controllers/PassengerController.java
+++ src/main/java/com/softraysolutions/basicpassengerservice/controllers/PassengerController.java
@@ -1,13 +1,19 @@
 package com.softraysolutions.basicpassengerservice.controllers;
 
+import com.softraysolutions.basicpassengerservice.exceptions.ResourceNotFoundException;
 import com.softraysolutions.basicpassengerservice.requests.PassengerRequest;
 import com.softraysolutions.basicpassengerservice.responses.PassengerResponse;
 import com.softraysolutions.basicpassengerservice.responses.Response;
 import com.softraysolutions.basicpassengerservice.services.PassengerService;
+import com.softraysolutions.basicpassengerservice.util.ErrorHandlingHelper;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLException;
 
 @AllArgsConstructor
 @RestController
@@ -19,6 +25,24 @@ public class PassengerController {
     public ResponseEntity<Response> createPassenger(@RequestBody PassengerRequest passengerRequest) {
         Response response = passengerService.addPassenger(passengerRequest);
         return ResponseEntity.ok(response);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response handleConstraintViolationException(ConstraintViolationException exception) {
+        return ErrorHandlingHelper.handleConstraintViolationException(exception);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response handleEntityNotFoundException(ResourceNotFoundException exception) {
+        return ErrorHandlingHelper.handleEntityNotFoundException(exception);
+    }
+
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Response handleIllegalArgumentException(SQLException exception) {
+        return ErrorHandlingHelper.handleIllegalArgumentException(exception);
     }
 
 }
