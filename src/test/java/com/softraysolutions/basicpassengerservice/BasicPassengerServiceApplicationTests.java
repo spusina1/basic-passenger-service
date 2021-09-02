8d1b4da2cfc795afc1ffc46f915a651cb344c4db
Samra <spusina1@etf.unsa.ba>
2021-09-02 12:50:42 +0200

    initial commit

---
+++ src/test/java/com/softraysolutions/basicpassengerservice/BasicPassengerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softraysolutions.basicpassengerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicPassengerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
